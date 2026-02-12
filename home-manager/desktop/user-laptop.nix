9181f3297a9fdc71bdf069aa763dee489d4a4651
rokokol <mailofilyusha@gmail.com>
2026-02-12 18:31:38 +0300

    portals

--- home-manager/desktop/user-laptop.nix
+++ home-manager/desktop/user-laptop.nix
@@ -6,33 +6,6 @@
   home.stateVersion = "25.11";
 
   home.packages = with pkgs; [
-    # hyprland & desktop
-    kitty
-    swww
-    hypridle
-    hyprlock
-    hyprpolkitagent
-    waybar
-    libsForQt5.qt5.qtwayland
-    qt6.qtwayland
-    brightnessctl
-    cliphist
-    grim
-    slurp
-    satty
-    hyprpicker
-    libnotify
-    seahorse
-    (symlinkJoin {
-      name = "pavucontrol";
-      paths = [ pavucontrol ];
-      buildInputs = [ makeWrapper ];
-      postBuild = ''
-        wrapProgram $out/bin/pavucontrol \
-          --set GTK_THEME Adwaita
-      '';
-    })
-
     # programs
     ayugram-desktop
     adwaita-icon-theme
@@ -41,7 +14,7 @@
     gnome-disk-utility
     celluloid
     swayimg
-    loupe
+    gthumb
     file-roller
     octaveFull
   ];
@@ -49,25 +22,7 @@
   home.sessionVariables = {
     NIXPKGS_ALLOW_UNFREE = "1";
     EDITOR = "nvim";
-    QT_QPA_PLATFORM = "wayland";
-    SDL_VIDEODRIVER = "wayland";
     TERMINAL = "kitty";
     BROWSER = "firefox";
-    SSH_ASKPASS = "${pkgs.seahorse}/libexec/seahorse/ssh-askpass";
-    SSH_ASKPASS_REQUIRE = "force";
-    SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/keyring/ssh";
-    GTK_THEME = "rose-pine-dawn";
-  };
-
-  gtk = {
-    enable = true;
-    theme = {
-      name = "rose-pine-dawn";
-      package = pkgs.rose-pine-gtk-theme;
-    };
-    iconTheme = {
-      name = "rose-pine-dawn";
-      package = pkgs.rose-pine-icon-theme;
-    };
   };
 }
