7d113f0a97d9ecda64c4a3cec28d99339060afb3
rokokol <mailofilyusha@gmail.com>
2026-02-21 01:06:02 +0300

    sync Сб 21 фев 2026 01:06:02 MSK from nixos-laptop

--- nixos/laptop/boot.nix
+++ nixos/laptop/boot.nix
@@ -1,6 +1,15 @@
 { ... }:
 
 {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+
+    tmp = {
+      useTmpfs = true;
+      tmpfsSize = "40G";
+    };
+  };
 }
--- nixos/pc/boot.nix
+++ nixos/pc/boot.nix
@@ -1,11 +1,17 @@
 { pkgs, ... }:
 
 {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
 
-  # Newest kernel
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  # boot.kernelParams = [ "usbcore.autosuspend=-1" ];
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    tmp = {
+      useTmpfs = true;
+      tmpfsSize = "40G";
+    };
+  };
 }
-
