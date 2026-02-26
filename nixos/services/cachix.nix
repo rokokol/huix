f4cf3a3bfc3537b87695d65b24c7dec81d9f122b
rokokol <mailofilyusha@gmail.com>
2026-02-26 23:00:49 +0300

    sync Чт 26 фев 2026 23:00:49 MSK from nixos-pc

--- nixos/services/cachix.nix
+++ nixos/services/cachix.nix
@@ -8,10 +8,10 @@
       "https://cache.nixos.org"
       "https://cuda-maintainers.cachix.org"
     ];
+
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPM97uXNo7Cqyf66IuU8Hk2oG0g8v8p6s="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     ];
   };
 }
