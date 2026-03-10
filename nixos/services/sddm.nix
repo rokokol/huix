9f2066d1f6ebc2ec3af7c32a874f0057c66eae4b
rokokol <mailofilyusha@gmail.com>
2026-03-10 22:00:26 +0300

    sync Вт 10 мар 2026 22:00:26 MSK from

--- nixos/services/sddm.nix
+++ nixos/services/sddm.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   services.displayManager.sddm = {
@@ -6,10 +6,21 @@
     wayland.enable = true;
     wayland.compositor = "kwin";
     theme = "catppuccin-mocha-maroon";
+
+    settings = {
+      Theme = {
+        CursorTheme = "breeze_cursors";
+        CursorSize = 24;
+      };
+    };
   };
 
   environment.variables = {
     KWIN_FORCE_SW_CURSOR = "1";
     WLR_NO_HARDWARE_CURSORS = "1";
   };
+
+  environment.systemPackages = with pkgs; [
+    kdePackages.breeze
+  ];
 }
