239594fe41b9afaea4af93481a043e926279a201
rokokol <mailofilyusha@gmail.com>
2026-03-13 12:19:57 +0300

    sync Пт 13 мар 2026 12:19:57 MSK from

--- home-manager/hyprland/hyprland-packages.nix
+++ home-manager/hyprland/hyprland-packages.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   services.swayosd.enable = true;
   services.playerctld.enable = true;
@@ -63,6 +63,22 @@
 
     gtk4.extraConfig = {
       gtk-application-prefer-dark-theme = 0;
+    };
+  };
+
+  specialisation.dark.configuration = {
+    gtk = {
+      # Используем mkForce для разрешения конфликта
+      theme.name = lib.mkForce "Gruvbox-Dark";
+
+      iconTheme.name = lib.mkForce "rose-pine-moon"; # Если иконки тоже меняются
+
+      gtk3.extraConfig = {
+        gtk-application-prefer-dark-theme = lib.mkForce 1;
+      };
+      gtk4.extraConfig = {
+        gtk-application-prefer-dark-theme = lib.mkForce 1;
+      };
     };
   };
 
