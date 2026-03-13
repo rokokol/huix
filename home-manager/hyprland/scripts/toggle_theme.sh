d1325839fa8591a342f0f03dec3760f9c05efbad
rokokol <mailofilyusha@gmail.com>
2026-03-13 14:02:08 +0300

    sync Пт 13 мар 2026 14:02:08 MSK from

--- home-manager/hyprland/scripts/toggle_theme.sh
+++ home-manager/hyprland/scripts/toggle_theme.sh
@@ -3,6 +3,10 @@
 THEME=$(dconf read /org/gnome/desktop/interface/gtk-theme)
 if [[ "${THEME,,}" == *"light"* ]]; then
   dconf write /org/gnome/desktop/interface/gtk-theme "'Gruvbox-Dark'"
+  notify-send -u low "Dark theme set 🌑"
 elif [[ "${THEME,,}" == *"dark"* ]]; then
   dconf write /org/gnome/desktop/interface/gtk-theme "'Gruvbox-Light'"
+  notify-send -u low "Light theme set 🌕"
+else
+  notify-send -u critical "Cannot determine theme ヽ(ﾟДﾟ)ﾉ"
 fi
