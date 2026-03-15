ae04b595a6ce33b5ec1db1873f0f3e74005a952d
rokokol <mailofilyusha@gmail.com>
2026-03-15 23:00:57 +0300

    sync Вс 15 мар 2026 23:00:57 MSK from

--- nixos/services/openwebui.nix
+++ nixos/services/openwebui.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   services.open-webui = {
@@ -11,5 +11,23 @@
     environment = {
       OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
     };
+  };
+
+  users.groups.open-webui = { };
+  users.users.open-webui = {
+    isSystemUser = true;
+    group = "open-webui";
+  };
+
+  # 2. Добавляем пользователя rokokol в группу open-webui
+  users.users.rokokol.extraGroups = [ "open-webui" ];
+
+  # 3. Переопределяем параметры изоляции systemd-сервиса
+  systemd.services.open-webui.serviceConfig = {
+    DynamicUser = lib.mkForce false; # Отключаем изоляцию в /var/lib/private
+    User = "open-webui";
+    Group = "open-webui";
+    StateDirectoryMode = "0770"; # Даем полные права владельцу и группе
+    UMask = "0007"; # Новые файлы будут создаваться с правами группы (rw-)
   };
 }
