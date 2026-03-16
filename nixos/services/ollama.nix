9da04f43d3b32f7dea065643971d51fd9b236027
rokokol <mailofilyusha@gmail.com>
2026-03-16 13:08:37 +0300

    sync Пн 16 мар 2026 13:08:37 MSK from

--- nixos/services/ollama.nix
+++ nixos/services/ollama.nix
@@ -9,8 +9,6 @@
     services.ollama = {
       enable = true;
       package = lib.mkDefault pkgs.ollama;
-      host = "0.0.0.0";
     };
-    networking.firewall.allowedTCPPorts = [ 11434 ];
   };
 }
