d8fffc0032c34bd7f87049ce443c3f38ccd8b9a0
rokokol <mailofilyusha@gmail.com>
2026-03-16 05:00:35 +0300

    sync Пн 16 мар 2026 05:00:35 MSK from

--- nixos/configuration-pc.nix
+++ nixos/configuration-pc.nix
@@ -2,7 +2,7 @@
 
 let
   # to avoid transformers build
-  myPython = pkgs.cuda.python3.override {
+  myPython = pkgs.stable.python3.override {
     packageOverrides = _: super: {
       torch = super.torch-bin;
       torchvision = super.torchvision-bin;
