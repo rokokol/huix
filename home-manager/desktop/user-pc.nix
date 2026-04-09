22f29c507be545701eb2fe64c46fb16687f9f5bb
rokokol <mailofilyusha@gmail.com>
2026-04-09 13:20:37 +0300

    sync Чт 09 апр 2026 13:20:37 MSK from

--- home-manager/desktop/user-pc.nix
+++ home-manager/desktop/user-pc.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -66,5 +66,12 @@
     TERMINAL = "kitty";
     BROWSER = "firefox";
     HUIX = "$HOME/huix";
+  };
+
+  xdg.dataFile = {
+    "v2rayN/bin/sing_box/sing-box".source = "${pkgs.sing-box}/bin/sing-box";
+    "v2rayN/bin/xray/xray".source = "${pkgs.xray}/bin/xray";
+    "v2rayN/bin/geoip.dat".source = "${pkgs.v2ray-geoip}/share/v2ray/geoip.dat";
+    "v2rayN/bin/geosite.dat".source = "${pkgs.v2ray-domain-list-community}/share/v2ray/geosite.dat";
   };
 }
