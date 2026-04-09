b77c013075947dbcaf9dabaaf7b8c658c733fb93
rokokol <mailofilyusha@gmail.com>
2026-04-09 14:11:51 +0300

    sync Чт 09 апр 2026 14:11:51 MSK from

--- nixos/pc/desktop.nix
+++ nixos/pc/desktop.nix
@@ -11,12 +11,18 @@
   # Portals (apps & system interaction)
   xdg.portal = {
     enable = true;
+    xdgOpenUsePortal = true;
     extraPortals = with pkgs; [
       xdg-desktop-portal-hyprland
       xdg-desktop-portal-gtk
     ];
 
     config.common = {
+      # Use GTK portal for chooser/open dialogs, keep Hyprland portal available
+      # for compositor-specific interfaces like screencasting.
+      "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+      "org.freedesktop.impl.portal.OpenURI" = [ "gtk" ];
+
       default = [
         "hyprland"
         "gtk"
