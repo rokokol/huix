4d92b5135247642f4e75f3aae5cc855621ccef6c
rokokol <mailofilyusha@gmail.com>
2026-04-13 21:54:56 +0300

    sync Пн 13 апр 2026 21:54:56 MSK from nixos-pc

--- home-manager/hyprland/scripts/rofi-clipboard.sh
+++ home-manager/hyprland/scripts/rofi-clipboard.sh
@@ -40,11 +40,11 @@ while true; do
       -i \
       -show-icons \
       -display-columns 2 \
-      -theme-str 'element-icon { size: 72px; } element { padding: 12px 14px; }' \
+      \
       -p "📋" \
       -mesg "Enter: копировать | Ctrl+d: удалить" \
       -kb-remove-char-forward "Delete" \
-      -kb-custom-1 "Control+d"
+      -kb-custom-1 "Control+d" # -theme-str 'element-icon { size: 72px; } element { padding: 12px 14px; }' \
   ); then
     status=0
   else
