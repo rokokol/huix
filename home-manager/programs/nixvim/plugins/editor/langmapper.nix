855d16bc9fceb85b5f1e44742d2080b5afa1a082
rokokol <mailofilyusha@gmail.com>
2026-04-15 03:00:17 +0300

    sync Ср 15 апр 2026 03:00:17 MSK from nixos-laptop

--- home-manager/programs/nixvim/plugins/editor/langmapper.nix
+++ home-manager/programs/nixvim/plugins/editor/langmapper.nix
@@ -1,7 +1,81 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
+
+let
+  langmap = lib.concatStringsSep "," [
+    "йq"
+    "цw"
+    "уe"
+    "кr"
+    "еt"
+    "нy"
+    "гu"
+    "шi"
+    "щo"
+    "зp"
+    "х["
+    "ъ]"
+    "фa"
+    "ыs"
+    "вd"
+    "аf"
+    "пg"
+    "рh"
+    "оj"
+    "лk"
+    "дl"
+    "ж\\;"
+    "э'"
+    "яz"
+    "чx"
+    "сc"
+    "мv"
+    "иb"
+    "тn"
+    "ьm"
+    "б\\,"
+    "ю."
+    "ЙQ"
+    "ЦW"
+    "УE"
+    "КR"
+    "ЕT"
+    "НY"
+    "ГU"
+    "ШI"
+    "ЩO"
+    "ЗP"
+    "Х{"
+    "Ъ}"
+    "ФA"
+    "ЫS"
+    "ВD"
+    "АF"
+    "ПG"
+    "РH"
+    "ОJ"
+    "ЛK"
+    "ДL"
+    "Ж:"
+    "Э\\\""
+    "ЯZ"
+    "ЧX"
+    "СC"
+    "МV"
+    "ИB"
+    "ТN"
+    "ЬM"
+    "Б<"
+    "Ю>"
+  ];
+in
 
 {
   programs.nixvim = {
+    opts = {
+      langmap = langmap;
+      langremap = false;
+    };
+
     extraPlugins = [
       pkgs.vimPlugins.langmapper-nvim
     ];
