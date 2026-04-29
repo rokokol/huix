58350dff2c06d1f87106806a5669bbbd39d93ee9
rokokol <mailofilyusha@gmail.com>
2026-04-29 03:11:53 +0300

    sync Ср 29 апр 2026 03:11:53 MSK from nixos-pc

--- nixos/desktop/portals.nix
+++ nixos/desktop/portals.nix
@@ -7,19 +7,19 @@
     enable = true;
     extraPortals = with pkgs; [
       xdg-desktop-portal-hyprland
-      xdg-desktop-portal-gnome
+      xdg-desktop-portal-gtk
     ];
 
-    config.hyprland = {
-      "org.freedesktop.impl.portal.FileChooser" = [ "gnome" ];
-      "org.freedesktop.impl.portal.OpenURI" = [ "gnome" ];
-      "org.freedesktop.impl.portal.Settings" = [ "gnome" ];
+    config.common = {
+      "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+      "org.freedesktop.impl.portal.OpenURI" = [ "gtk" ];
+      "org.freedesktop.impl.portal.Settings" = [ "gtk" ];
       "org.freedesktop.impl.portal.ScreenCast" = [ "hyprland" ];
       "org.freedesktop.impl.portal.Screenshot" = [ "hyprland" ];
 
       default = [
         "hyprland"
-        "gnome"
+        "gtk"
       ];
     };
   };
