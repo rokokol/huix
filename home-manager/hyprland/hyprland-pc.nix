6aec0c6884830de3d9fcdac60825552d87062b3a
rokokol <mailofilyusha@gmail.com>
2026-04-29 10:41:17 +0300

    sync Ср 29 апр 2026 10:41:17 MSK from nixos-pc

--- home-manager/hyprland/hyprland-pc.nix
+++ home-manager/hyprland/hyprland-pc.nix
@@ -20,13 +20,6 @@
     };
   };
 
-  home.file.".config/hypr/xdph.conf".text = ''
-    screencopy {
-      # Work around DMA-BUF allocation failures seen on the NVIDIA desktop.
-      force_shm = true
-    }
-  '';
-
   imports = [
     ./hyprland-packages.nix
     ./waybar-pc.nix
