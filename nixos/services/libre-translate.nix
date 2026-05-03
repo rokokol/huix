ecad0564ccfbb570566862b6090b03ded9f57f9c
rokokol <mailofilyusha@gmail.com>
2026-05-03 14:00:22 +0300

    sync Вс 03 мая 2026 14:00:22 MSK from nixos-pc

---
+++ nixos/services/libre-translate.nix
@@ -0,0 +1,14 @@
+{ ... }:
+
+{
+  services.libretranslate = {
+    enable = true;
+    port = 5000;
+    updateModels = true;
+
+    extraArgs = [
+      "--load-only"
+      "ru,en"
+    ];
+  };
+}
