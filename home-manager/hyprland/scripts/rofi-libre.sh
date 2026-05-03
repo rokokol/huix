79f0ddf05ea4b5f539b99c45d421116c3f6e8dfe
rokokol <mailofilyusha@gmail.com>
2026-05-03 21:00:57 +0300

    sync Вс 03 мая 2026 21:00:57 MSK from nixos-pc

--- home-manager/hyprland/scripts/rofi-libre.sh
+++ home-manager/hyprland/scripts/rofi-libre.sh
@@ -5,25 +5,31 @@ TGT="$2"
 shift 2
 USER_INPUT="$*"
 
-# 1. Сценарий: Первый запуск (отрисовка)
+print_message() {
+  printf '\0message\x1f%s\n' "$1"
+}
+
+# Первый запуск
 if [[ -z "$USER_INPUT" ]]; then
+  print_message "$SRC -> $TGT ⊂(‘ω’⊂ )))Σ≡=─༄༅༄༅༄༅༄༅༄༅"
   exit 0
 fi
 
-# 2. Сценарий: Нажат Enter на готовом переводе
+# Нажат Enter на готовом переводе
 if [[ "$USER_INPUT" == "✔ "* ]]; then
   # Убираем префикс "✔ " и копируем чистый текст
   echo -n "${USER_INPUT#✔ }" | wl-copy
   exit 0
 fi
 
-# 3. Сценарий: Введен новый текст (нужен перевод)
+# Введен новый текст (нужен перевод)
 translation=$(curl -s -L -X POST "http://localhost:$LIBRE_TRANSLATE_PORT/translate" \
   -H "Content-Type: application/json" \
   -d "{\"q\": \"$USER_INPUT\", \"source\": \"$SRC\", \"target\": \"$TGT\", \"format\": \"text\"}" | jq -r '.translatedText')
 
 if [[ -n "$translation" && "$translation" != "null" ]]; then
   # Выводим результат с префиксом, чтобы поймать его при следующем Enter
+  print_message "From \"$USER_INPUT\" o(^▽^)o"
   echo "✔ $translation"
 else
   echo "✖ Ошибка перевода ┬┴┬┴┤(･_├┬┴┬┴"
