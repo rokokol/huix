084faf20c19f18b29f72b93deb4e2de8f07afb57
rokokol <mailofilyusha@gmail.com>
2026-05-07 18:24:02 +0300

    sync Чт 07 мая 2026 18:24:02 MSK from nixos-pc

--- nixos/services/tools/libre-translate.nix
+++ nixos/services/tools/libre-translate.nix
@@ -7,7 +7,7 @@ in
   services.libretranslate = {
     enable = true;
     port = port;
-    updateModels = false;
+    updateModels = true;
 
     extraArgs = {
       "load-only" = "ru,en";
@@ -15,34 +15,6 @@ in
   };
 
   environment.systemPackages = [ pkgs.libretranslate ];
-
-  systemd.services.libretranslate-warmup = {
-    description = "Warm up LibreTranslate models in RAM";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "libretranslate.service" ];
-    requires = [ "libretranslate.service" ];
-
-    serviceConfig = {
-      Type = "oneshot";
-      TimeoutSec = 120;
-    };
-
-    script = ''
-      until ${pkgs.curl}/bin/curl -f -s "http://127.0.0.1:${toString port}/" > /dev/null; do
-        sleep 2
-      done
-
-      echo "Starting EN -> RU warmup..."
-      ${pkgs.curl}/bin/curl -f -X POST "http://127.0.0.1:${toString port}/translate" \
-        -H "Content-Type: application/json" \
-        -d '{"q": "warmup", "source": "en", "target": "ru"}'
-
-      echo -e "\nStarting RU -> EN warmup..."
-      ${pkgs.curl}/bin/curl -f -X POST "http://127.0.0.1:${toString port}/translate" \
-        -H "Content-Type: application/json" \
-        -d '{"q": "прогрев", "source": "ru", "target": "en"}'
-    '';
-  };
 
   environment.sessionVariables = {
     LIBRE_TRANSLATE_PORT = port;
