c38c5e04f31fb0d03fddc354d8d74a1fcba86c33
rokokol <mailofilyusha@gmail.com>
2026-05-09 14:58:54 +0300

    sync Сб 09 мая 2026 14:58:54 MSK from nixos-pc

--- nixos/configuration-pc.nix
+++ nixos/configuration-pc.nix
@@ -1,15 +1,5 @@
 { pkgs, ... }:
 
-let
-  # to avoid transformers build
-  myPython = pkgs.stable.python3.override {
-    packageOverrides = _: super: {
-      torch = super.torch-bin;
-      torchvision = super.torchvision-bin;
-      torchaudio = super.torchaudio-bin;
-    };
-  };
-in
 {
   imports = [
     ./default.nix
@@ -18,25 +8,5 @@ in
   ];
 
   system.stateVersion = "25.11";
-  services.jupyter = {
-    pythonInterpreter = myPython;
-    pythonPackages = with myPython.pkgs; [
-      matplotlib
-      pandas
-      seaborn
-      numpy
-      sympy
-      librosa
-
-      scikit-learn
-      transformers
-      torch-bin
-      torchvision-bin
-      torchaudio-bin
-
-      pytesseract
-      ipywidgets
-    ];
-  };
   services.ollama.package = pkgs.ollama-cuda;
 }
--- nixos/services/tools/jupyter.nix
+++ nixos/services/tools/jupyter.nix
@@ -1,84 +1,80 @@
-{
-  config,
-  pkgs,
-  lib,
-  rokokolName,
-  ...
-}:
+{ pkgs, rokokolName, ... }:
 
 let
   homeDir = "/home/${rokokolName}";
-  python-datascience = config.services.jupyter.pythonInterpreter.withPackages (
-    ps:
-    with ps;
-    [
+  # to avoid transformers build
+  myPython = pkgs.stable.python3.override {
+    packageOverrides = _: super: {
+      torch = super.torch-bin;
+      torchvision = super.torchvision-bin;
+      torchaudio = super.torchaudio-bin;
+    };
+  };
+  pythonDatascience = myPython.withPackages (
+    ps: with ps; [
+      matplotlib
+      pandas
+      seaborn
+      numpy
+      sympy
+      librosa
+      scikit-learn
+      transformers
+      torch
+      torchvision
+      torchaudio
+      pytesseract
+      ipywidgets
       ipykernel
     ]
-    ++ config.services.jupyter.pythonPackages
   );
 in
 {
-  options.services.jupyter = {
-    pythonInterpreter = lib.mkOption {
-      type = lib.types.package;
-      default = pkgs.python3;
-      description = "Python interpreter for Jupyter";
-    };
+  services.jupyter = {
+    enable = true;
+    user = rokokolName;
+    group = "users";
+    command = "jupyter-lab";
+    notebookDir = "${homeDir}/Notebooks";
+    password = "argon2:$argon2id$v=19$m=10240,t=10,p=8$QQIsyCtNwAb7GSPc4f/fsQ$dJMkGhSyoVxKje2lMomM8mD0Y62GROuZOF1IzZwbZwo";
+    ip = "127.0.0.1";
+    port = 8888;
 
-    pythonPackages = lib.mkOption {
-      type = lib.types.listOf lib.types.package;
-      default = [ ];
-      description = "Additional Python packages";
-    };
-  };
+    notebookConfig = ''
+      c.KernelSpecManager.ensure_native_kernel = False
 
-  config = {
-    services.jupyter = {
-      enable = true;
-      user = rokokolName;
-      group = "users";
-      command = "jupyter-lab";
-      notebookDir = "${homeDir}/Notebooks";
-      password = "argon2:$argon2id$v=19$m=10240,t=10,p=8$QQIsyCtNwAb7GSPc4f/fsQ$dJMkGhSyoVxKje2lMomM8mD0Y62GROuZOF1IzZwbZwo";
-      ip = "127.0.0.1";
-      port = 8888;
+      visible_kernels = {'pythonDatascience', 'octave'}
+      c.KernelSpecManager.whitelist = visible_kernels
+    '';
 
-      notebookConfig = ''
-        c.KernelSpecManager.ensure_native_kernel = False
+    kernels = {
+      # clojure = pkgs.clojupyter.definition;
 
-        visible_kernels = {'python-datascience', 'octave'}
-        c.KernelSpecManager.whitelist = visible_kernels
-      '';
+      octave = pkgs.octave-kernel.definition;
 
-      kernels = {
-        # clojure = pkgs.clojupyter.definition;
-
-        octave = pkgs.octave-kernel.definition;
-
-        python-datascience = {
-          displayName = "Python (Data Science)";
-          argv = [
-            "${python-datascience.interpreter}"
-            "-m"
-            "ipykernel_launcher"
-            "-f"
-            "{connection_file}"
-          ];
-          language = "python";
-          logo32 = "${python-datascience}/${python-datascience.sitePackages}/ipykernel/resources/logo-32x32.png";
-          logo64 = "${python-datascience}/${python-datascience.sitePackages}/ipykernel/resources/logo-64x64.png";
-        };
+      pythonDatascience = {
+        displayName = "Python (Data Science)";
+        argv = [
+          "${pythonDatascience.interpreter}"
+          "-m"
+          "ipykernel_launcher"
+          "-f"
+          "{connection_file}"
+        ];
+        language = "python";
+        logo32 = "${pythonDatascience}/${pythonDatascience.sitePackages}/ipykernel/resources/logo-32x32.png";
+        logo64 = "${pythonDatascience}/${pythonDatascience.sitePackages}/ipykernel/resources/logo-64x64.png";
       };
     };
-
-    systemd.services.jupyter.path = with pkgs; [
-      octave
-      gnuplot
-      ghostscript
-    ];
-
-    systemd.tmpfiles.rules = [
-      "d ${homeDir}/Notebooks 0755 ${rokokolName} users -"
-    ];
   };
+
+  systemd.services.jupyter.path = with pkgs; [
+    octave
+    gnuplot
+    ghostscript
+  ];
+
+  systemd.tmpfiles.rules = [
+    "d ${homeDir}/Notebooks 0755 ${rokokolName} users -"
+  ];
 }
