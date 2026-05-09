03497200c2ddea6b4042be127f7108bf184c357f
rokokol <mailofilyusha@gmail.com>
2026-05-09 14:39:41 +0300

    sync Сб 09 мая 2026 14:39:41 MSK from nixos-pc

--- nixos/services/utils/zapret.nix
+++ nixos/services/utils/zapret.nix
@@ -23,9 +23,9 @@
       "--new"
       "--filter-tcp=443 --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-fake-tls=${pkgs.zapret}/usr/share/zapret/files/fake/tls_clienthello_www_google_com.bin"
       "--new"
-      "--filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-any-protocol --dpi-desync-fake-quic=${pkgs.zapret}/usr/share/zapret/files/fake/quic_initial_www_google_com.bin"
+      "--filter-udp=443 --dpi-desync=fake --dpi-desync-cutoff=n4 --dpi-desync-repeats=6 --dpi-desync-any-protocol --dpi-desync-fake-quic=${pkgs.zapret}/usr/share/zapret/files/fake/quic_initial_www_google_com.bin"
       "--new"
-      "--filter-udp=50000:65535 --dpi-desync=fake,tamper --dpi-desync-any-protocol --dpi-desync-repeats=6 --dpi-desync-fake-quic=${pkgs.zapret}/usr/share/zapret/files/fake/quic_initial_www_google_com.bin"
+      "--filter-udp=50000-65535 --dpi-desync=fake,tamper --dpi-desync-cutoff=n4 --dpi-desync-any-protocol --dpi-desync-repeats=6 --dpi-desync-fake-quic=${pkgs.zapret}/usr/share/zapret/files/fake/quic_initial_www_google_com.bin"
     ];
   };
 }
