03e4361c7b64bfa72020de00ed879c733c0f13b4
rokokol <mailofilyusha@gmail.com>
2026-05-09 08:44:38 +0300

    sync Сб 09 мая 2026 08:44:38 MSK from nixos-pc

---
+++ nixos/services/utils/zapret.nix
@@ -0,0 +1,12 @@
+{ ... }:
+
+{
+  services.zapret = {
+    enable = true;
+    params = [
+      "--filter-tcp=443 --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-repeats=6 --dpi-desync-fooling=md5sig"
+      "--new"
+      "--filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-any-protocol"
+    ];
+  };
+}
