16e8c5a7e53889e2d90f60174fa64c1f252be27e
rokokol <mailofilyusha@gmail.com>
2026-05-09 12:40:23 +0300

    sync Сб 09 мая 2026 12:40:23 MSK from nixos-pc

--- nixos/services/utils/zapret.nix
+++ nixos/services/utils/zapret.nix
@@ -1,17 +1,35 @@
 { ... }:
 
 {
+  boot.kernelModules = [
+    "nft_queue"
+    "nfnetlink_queue"
+  ];
+
+  networking.enableIPv6 = false;
+
   services.zapret = {
     enable = true;
+
+    udpSupport = true;
+    udpPorts = [ "443" ];
+
+    # blacklist = [
+    #   "googlevideo.com"
+    #   "youtube.com"
+    #   "youtubei.googleapis.com"
+    #   "ytimg.com"
+    #   "ggpht.com"
+    #   "discord.com"
+    #   "discordapp.com"
+    #   "discordapp.net"
+    #   "discord.gg"
+    # ];
+
     params = [
       "--filter-tcp=443 --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-repeats=6 --dpi-desync-fooling=md5sig"
       "--new"
       "--filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-any-protocol"
     ];
   };
-
-  boot.kernelModules = [
-    "nft_queue"
-    "nfnetlink_queue"
-  ];
 }
