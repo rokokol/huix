6e169b2729031b41f468a807389e86c48364750b
rokokol <mailofilyusha@gmail.com>
2026-05-11 18:04:26 +0300

    sync Пн 11 мая 2026 18:04:26 MSK from nixos-laptop

--- nixos/services/services-laptop.nix
+++ nixos/services/services-laptop.nix
@@ -1,32 +1,16 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
-  boot.kernelModules = [
-    "nft_queue"
-    "nfnetlink_queue"
+  imports = [
+    ./ai/ollama.nix
+    ./desktop/amnezia-vpn.nix
+    ./desktop/file-manager.nix
+    ./desktop/sddm.nix
+    ./desktop/ssh-askpass.nix
+    ./utils/tor.nix
+    ./desktop/wl-clip-persist.nix
+    ./system/cachix.nix
+    ./tools/libre-translate.nix
+    ./tools/syncthing.nix
   ];
-
-  networking.enableIPv6 = false;
-
-  # Дропаем UDP/443 на уровне системы, чтобы 100% форсировать TLS (TCP)
-  networking.firewall.extraCommands = ''
-    iptables -I OUTPUT -p udp --dport 443 -j REJECT
-  '';
-
-  services.zapret = {
-    enable = true;
-    package = pkgs.zapret;
-
-    # Отключаем UDP в самом zapret
-    udpSupport = false;
-
-    # Пробуем syndata (эффективнее на многих ТСПУ) и фиксированный TTL (если нужно - раскомментировать)
-    params = [
-      "--filter-tcp=80 --dpi-desync=fake,syndata --dpi-desync-fooling=md5sig"
-      "--new"
-      "--filter-tcp=443 --dpi-desync=fake,syndata --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-fake-tls=${pkgs.zapret}/usr/share/zapret/files/fake/tls_clienthello_www_google_com.bin"
-      # Если syndata не сработает, попробуй добавить фиксированный TTL:
-      # "--filter-tcp=443 --dpi-desync=fake,split2 --dpi-desync-ttl=3 --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-fake-tls=${pkgs.zapret}/usr/share/zapret/files/fake/tls_clienthello_www_google_com.bin"
-    ];
-  };
 }
