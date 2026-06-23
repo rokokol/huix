744c6d4ca1281cc2d358478f82881ca7ac7beaf0
rokokol <mailofilyusha@gmail.com>
2026-06-23 18:21:06 +0300

    sync Вт 23 июн 2026 18:21:06 MSK from nixos-pc

--- home-manager/desktop/hyprland/hyprland-pc.nix
+++ home-manager/desktop/hyprland/hyprland-pc.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ huixDir, ... }:
 
 {
   wayland.windowManager.hyprland = {
@@ -19,6 +19,18 @@
 
         sensitivity = 0;
       };
+
+      # Софт-яркость через шейдер: у десктоп-монитора нет аппаратной подсветки,
+      # поэтому затемняем картинку шейдером Hyprland.
+      # Модификаторы пишем литералом (SUPER ALT), т.к. эти настройки
+      # рендерятся ДО source = hyprland.conf, где определён $mainScreenMod.
+      bindel = [
+        "SUPER ALT, bracketright, exec, ${huixDir}/scripts/screen-shader.sh bright up"
+        "SUPER ALT, bracketleft,  exec, ${huixDir}/scripts/screen-shader.sh bright down"
+      ];
+      bind = [
+        "SUPER ALT, backslash, exec, ${huixDir}/scripts/screen-shader.sh bright reset"
+      ];
     };
   };
 
