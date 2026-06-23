ede636f795dc71e6411fabf2f841e9011fa80782
rokokol <mailofilyusha@gmail.com>
2026-06-23 17:00:09 +0300

    sync Вт 23 июн 2026 17:00:09 MSK from nixos-pc

--- nixos/services/devices/tablet.nix
+++ nixos/services/devices/tablet.nix
@@ -8,4 +8,13 @@
 
   boot.kernelModules = [ "uinput" ];
   boot.blacklistedKernelModules = [ "hid-uclogic" ];
+
+  # Скрыть сырой планшет GAOMON S630 (256c:006d) от libinput/Hyprland: курсором
+  # рулит только виртуальный девайс OpenTabletDriver. Иначе сырой планшет летит
+  # как mouse0 и дерётся с OTD за абсолютную позицию — курсор залипает, обычная
+  # мышь перестаёт работать, клики пером пропадают. OTD читает планшет напрямую
+  # через hidraw/usb, поэтому продолжает работать.
+  services.udev.extraRules = ''
+    SUBSYSTEM=="input", ATTRS{idVendor}=="256c", ATTRS{idProduct}=="006d", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+  '';
 }
