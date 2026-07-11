8d3ec56f6861811b7cf96e1b7965ccfc0577513d
rokokol <mailofilyusha@gmail.com>
2026-07-11 19:37:15 +0300

    sync Сб 11 июл 2026 19:37:15 MSK from nixos-laptop

--- home-manager/desktop/theme/cursor.nix
+++ home-manager/desktop/theme/cursor.nix
@@ -1,11 +1,25 @@
 { pkgs, ... }:
+# Sayori Cursor V2 — анимированная тема курсора в стиле DDLC
+# Автор: sev (https://ko-fi.com/sevverae)
+# Оригинал: https://ko-fi.com/s/8e05db90c4
 
+let
+  sayori-cursor = pkgs.stdenv.mkDerivation {
+    name = "sayori-cursor-v2";
+    src = ../../../assets/sayori-cursor-v2;
+    dontUnpack = true;
+    installPhase = ''
+      mkdir -p $out/share/icons/Sayori-Cursor-V2
+      cp -a $src/* $out/share/icons/Sayori-Cursor-V2/
+    '';
+  };
+in
 {
   home.pointerCursor = {
     gtk.enable = true;
     x11.enable = true;
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Ice";
+    package = sayori-cursor;
+    name = "Sayori-Cursor-V2";
     size = 20;
   };
 }
