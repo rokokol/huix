a11c8c3ce5e0ba839ee7bcfed70589cb0c23a2c9
rokokol <mailofilyusha@gmail.com>
2026-07-19 18:06:12 +0300

    sync Вс 19 июл 2026 18:06:12 MSK from nixos-laptop

--- home-manager/desktop/hyprland/services/hyprland-packages.nix
+++ home-manager/desktop/hyprland/services/hyprland-packages.nix
@@ -63,6 +63,7 @@
   # больше не читается, поэтому и info-оверлей, и биндинги задаём здесь.
   home.file.".config/swayimg/init.lua".text = ''
     swayimg.set_mode("viewer")
+    swayimg.viewer.set_scale("fit")
 
     -- По умолчанию никакого текстового оверлея: пустые схемы для всех углов.
     swayimg.viewer.set_text("topleft", {})
